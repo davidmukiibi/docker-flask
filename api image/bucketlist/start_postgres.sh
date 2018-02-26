5c6fa22fadb041db2bcdd686583d1e725d09bd61
davidmukiibi <david.mukiibiq@gmail.com>
2018-02-26 15:33:22 +0300

    Rearraneg comments

--- api image/bucketlist/start_postgres.sh
+++ api image/bucketlist/start_postgres.sh
@@ -1,11 +1,4 @@
 #!/usr/bin/env bash
-
-# this bash script checks port number 5432 of container named amity-db
-# to ascertain that it is open. this means that the postgres server is up and running
-# amity-db is what i named the postgres database container.
-# when the port is open, then the script run all other subsequent commands following
-# which commands are essentially running migrations on the database in that said container
-# and then start the flask API application.
 set -e
 
 echo "Waiting for postgres..."
@@ -16,12 +9,18 @@ done
 
 echo "PostgreSQL started"
 
-
 python manage.py db init
 python manage.py db migrate
 python manage.py db upgrade
 
 python run.py
+
+# this bash script checks port number 5432 of container named amity-db
+# to ascertain that it is open. this means that the postgres server is up and running
+# amity-db is what i named the postgres database container.
+# when the port is open, then the script run all other subsequent commands following
+# which commands are essentially running migrations on the database in that said container
+# and then start the flask API application.
 
 
 
